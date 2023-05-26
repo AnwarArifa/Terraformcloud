1ce4d6cb1d085b207909299884472fa7184d234b
AnwarArifa <anwar.sails@gmail.com>
2023-05-26 22:13:20 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
 provider "aws" {
-  region     = "us-east-1"
-  access_key = ""
-  secret_key = ""
+  region = "us-east-1"
 }
 
 resource "aws_instance" "myec2" {
