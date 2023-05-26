b8e8f65d5e657ff0bacc2a92ed1fc363a823b8c8
AnwarArifa <anwar.sails@gmail.com>
2023-05-26 22:40:56 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -3,7 +3,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "myec2" {
+resource "aws_instance" "my_example" {
     ami = "ami-053b0d53c279acc90"
     instance_type = "t2.micro"
     subnet_id = "subnet-0fe60cfc8d842e851"
